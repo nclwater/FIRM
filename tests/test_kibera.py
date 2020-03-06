d360e9456b17eab3c6c013c92a00a42340e159f0
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2020-03-06 09:28:07 +0000

    updated kibera test

--- tests/test_kibera.py
+++ tests/test_kibera.py
@@ -1,89 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 import run
+import scenario
 import os
+from utils import read_netlogo_representation
 
+agents = read_netlogo_representation('tests/kibera/agents.txt')
 
-agents = [
-    ["kids", "home 1", [
-        ["home 1", "daily 6:05 15m", "school",  0.9],
-        ["home 1", "daily 6:05 3h", "some other", 0.1],
-        ["school", "daily 15:00 30m", "home 1", 0.5],
-        ["school", "daily 15:00 30m", "some other", 0.5],
-        ["some other", "3h 1h", "home 1", 1],
-        ["evacuate", "5m 1m", "some evacuation point", 0.9],
-        ["evacuate", "0s 0s", "resume", 0.1],
-        ["some evacuation point", "daily 4h 1h", "home 1", 1]
-     ]],
-    ["teenagers", "home 1", [
-        ["home 1", "daily 6:05 15m", "school",  0.58],
-        ["home 1", "daily 6:05 1h", "work",  0.2],
-        ["home 1", "daily 6:05 3h", "some other", 0.22],
-        ["work", "daily 8:00 1h", "some other", 0.3],
-        ["work", "daily 8:00 3h", "home 1", 0.2],
-        ["school", "daily 15:00 30m", "home 1", 0.5],
-        ["school", "daily 15:00 30m", "some other", 0.5],
-        ["work", "daily 16:00 10m", "home 1", 1],
-        ["some other", "3h 1h", "home 1", 1],
-        ["evacuate", "5m 1m", "some evacuation point", 0.9],
-        ["evacuate", "0s 0s", "resume", 0.1],
-        ["some evacuation point", "daily 4h 1h", "home 1", 1]
-    ]],
-    ["Men", "home 1", [
-        ["home 1", "daily 6:05 0m", "some work",  0.6],
-        ["home 1", "daily 6:05 1h", "some other", 0.4],
-        ["some other", "daily 16:00 5m", "some other", 1],
-        ["some work", "daily 16:00 5m", "home 1", 0.6],
-        ["some work", "daily 16:00 5m", "some shop", 0.2],
-        ["some work", "daily 16:00 5m", "some other", 0.2],
-        ["some other", "3h 1h", "home 1", 1],
-        ["some shop", "3h 1h", "home 1", 1],
-        ["evacuate", "5m 1m", "some evacuation point", 0.9],
-        ["evacuate", "0s 0s", "resume", 0.1],
-        ["some evacuation point", "daily 4h 1h", "home 1", 1],
-    ]],
-    ["Women", "home 1", [
-        ["home 1", "daily 6:05 0m", "school",  0.5],
-        ["home 1", "daily 6:05 0m", "some shop", 0.5],
-        ["school", "1h 20m", "home 2", 0.9],
-        ["school", "30m 1m", "some shop", 0.1],
-        ["home 2", "daily 16:00 0m", "school", 1],
-        ["some shop", "daily 16:00 5m", "home 1", 0.6],
-        ["some shop", "daily 16:00 5m", "some other", 0.4],
-        ["school", "30m 0m", "home 1", 1],
-        ["some other", "3h 1h", "home 1", 1],
-        ["evacuate", "5m 1m", "some evacuation point", 0.9],
-        ["evacuate", "0s 0s", "resume", 0.1],
-        ["some evacuation point", "daily 4h 1h", "home 1", 1]
-    ]],
-    ["transit eastbound", "Road1", [
-        ["Road1", "0s 0s", "Road2", 1],
-        ["Road2", "0s 0s", "exit", 1]
-    ]],
-    ["transit westbound", "Road2", [
-        ["Road2", "0s 0s", "Road3", 1],
-        ["Road3", "0s 0s", "exit", 1]
-    ]],
-    ["transit northbound", "Road3", [
-        ["Road3", "0s 0s", "Road1", 1],
-        ["Road1", "0s 0s", "exit", 1]
-    ]],
-    ["test2", "home", [
-        ["home", "0m 0m", "school", 1],
-        ["school", "0m 0m", "home", 1],
-        ["evacuate", "0m 0m", "some evacuation point", 0.9],
-        ["evacuate", "0s 0s", "resume", 0.1],
-        ["some evacuation point", "0m 0m", "home", 1]
-    ]]
-]
+defences_to_breach = ["a"]
 
-defences = ["a", "b", "c"]
+defences = read_netlogo_representation('tests/kibera/defences.txt')
+
+roads = read_netlogo_representation('tests/kibera/roads.txt')
+
+codes = read_netlogo_representation('tests/kibera/codes.txt')
+
+terrain = read_netlogo_representation('tests/kibera/terrain.txt')
+
+buildings = read_netlogo_representation('tests/kibera/preprocessed-buildings.txt')
+
+streams = read_netlogo_representation('tests/kibera/streams.txt')
+
 
 warning_times = ["07:50:00", "07:55:00", "08:00:00"]
 
-sea_levels = [4, 5, 6]
+sea_levels = [4]
 
 if not os.path.exists('tests/outputs'):
     os.mkdir('tests/outputs')
@@ -91,52 +33,57 @@ if not os.path.exists('tests/outputs'):
 model_path = 'tests/outputs/kibera'
 inputs_path = 'tests/kibera'
 
-r = run.Run(model_path=model_path,
-            codes_path=os.path.join(inputs_path, 'codes.txt'),
-            defences_path=os.path.join(inputs_path, 'defences.txt'),
-            preprocessed_buildings_path=os.path.join(inputs_path, 'preprocessed-buildings.txt'),
-            roads_path=os.path.join(inputs_path, 'roads.txt'),
-            streams_path=os.path.join(inputs_path, 'streams.txt'),
-            terrain_path=os.path.join(inputs_path, 'terrain.txt'),
-            agents=agents,
-            width=297,
-            height=104,
-            start_time='7:45',
-            end_time='8:20')
+scenarios = []
 
 
-for defence in defences:
+for defence in defences_to_breach:
     for warning_time in warning_times:
         for sea_level in sea_levels:
-            r.setup_and_run(name='{}-{}-{}'.format(defence, warning_time.replace(':', '.'), sea_level),
-                            timeline=
+            scenarios.append(
+                scenario.Scenario(
+                    path='tests/outputs/kibera-defence-{}-evacuation-{}-sea-{}'.format(defence, warning_time.replace(':', ''), sea_level),
+                    width=297,
+                    height=104,
+                    start_time='7:45',
+                    end_time='8:20',
+                    agents=agents,
+                    roads=roads,
+                    codes=codes,
+                    defences=defences,
+                    buildings=buildings,
+                    terrain=terrain,
+                    streams=streams,
+                    timeline=
                     [
-                    [
                         [
-                            ["normal", "08:00", "15m"],
-                            500,
-                            ["agent", "transit eastbound"],
-                            0.8,
-                            ["agent", "transit westbound"],
-                            0.2
-                        ],
-                        [
-                            "0s",
-                            10, # number of agents
-                            ["agent", "kids"]
-                        ],
-                        [
-                            "07:54",
-                            ["sealevel", sea_level]
-                        ],
-                        [
-                            "07:55",
-                            ["breach", defence]
-                        ],
-                        [
-                            warning_time,
-                            ["evacuate"]
+                            [
+                                ["normal", "08:00", "15m"],
+                                500,
+                                ["agent", "transit eastbound"],
+                                0.8,
+                                ["agent", "transit westbound"],
+                                0.2
+                            ],
+                            [
+                                "0s",
+                                10, # number of agents
+                                ["agent", "kids"]
+                            ],
+                            [
+                                "07:54",
+                                ["sealevel", sea_level]
+                            ],
+                            [
+                                "07:55",
+                                ["breach", defence]
+                            ],
+                            [
+                                warning_time,
+                                ["evacuate"]
+                            ]
                         ]
-                    ]
-                ]
-                            )
+                    ]))
+
+r = run.Run(scenarios=scenarios)
+
+r.setup_and_run()
