b0289d1b64b8fc7e4a9302dce5955916e4514050
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2020-03-06 12:06:55 +0000

    added docstrings

--- scenario.py
+++ scenario.py
@@ -4,19 +4,74 @@ import os
 
 class Scenario:
     def __init__(self,
-                 path,
-                 width,
-                 height,
-                 start_time,
-                 end_time,
-                 agents,
-                 timeline,
-                 codes,
-                 defences,
-                 buildings,
-                 roads,
-                 terrain,
-                 streams=None):
+                 path: str,
+                 width: int,
+                 height: int,
+                 start_time: str,
+                 end_time: str,
+                 agents: list,
+                 timeline: list,
+                 codes: list,
+                 defences: list,
+                 buildings: list,
+                 roads: list,
+                 terrain: list,
+                 streams: list = None):
+        """
+
+        :param path: folder to put all the input and output files
+        :param width: width of the domain
+        :param height: height of the domain
+        :param start_time: start time
+        :param end_time: end time
+        :param agents: definitions of agent types
+
+            [
+                [
+                    agent_type, origin, <list of events>
+                        [origin, frequency, time, +-duration, destination, probability]
+                ...]
+            ... ]
+
+            For events occurring at the same time, a choice is made using the probabilities
+
+        :param timeline: sequence of events to take place
+
+
+            [
+
+                # either
+
+                [
+                    [[probability_distribution, time, +-duration], number],
+                    [["agent", type], probability],
+                    [["agent", type], probability]
+                    ...
+                ]
+
+                # or
+
+                [time, number, ["agent", type]]
+
+                # or
+
+                [time, [function, argument]]
+
+                # or
+
+                [time, [function]
+
+                ...
+
+            ]
+
+        :param codes: names for building and road types
+        :param defences: geometries of flood defences
+        :param buildings: geometries of buildings
+        :param roads: geometries of roads
+        :param terrain: digital elevation model
+        :param streams: gridded representation of streams
+        """
         self.path = path
         self.width = width
         self.height = height
@@ -48,7 +103,12 @@ class Scenario:
             if sequence is not None:
                 self.write_data_file(filename, sequence)
 
-    def write_data_file(self, filename, sequence):
+    def write_data_file(self, filename: str, sequence: list):
+        """
+
+        :param filename: name of output file
+        :param sequence: data to store in output file
+        """
         if not os.path.exists(self.path):
             os.mkdir(self.path)
         with open(self.path + "/" + filename, "w") as f:
