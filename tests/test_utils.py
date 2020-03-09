51e02e7e3a083ba2ae22a4b94ad81555265a0979
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2020-03-09 18:26:30 +0000

    added convert_terrain in utils, fixed read_netlogo_representation

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,5 @@
 import utils
 
 utils.read_netlogo_representation('tests/towyn/agents.txt')
+
+utils.convert_terrain('tests/bwaise/terrain.asc', 'tests/bwaise/terrain.txt')
--- utils.py
+++ utils.py
@@ -3,26 +3,34 @@ import ast
 import re
 
 
-def netlogo_representation(sequence: list):
+def netlogo_representation(sequence: list, **kwargs):
     """
 
     :param sequence:
     :return:
     """
 
-    return pprint.pformat(sequence).replace(',', ' ').replace('"', '\\').replace("'", '"')[1:-1]
+    return pprint.pformat(sequence, **kwargs).replace(',', ' ').replace('"', '\\').replace("'", '"')[1:-1]
 
 
 def read_netlogo_representation(path):
     with open(path) as f:
         lines = f.readlines()
 
-    lines = [line for line in lines if not line.startswith(';')]
+    lines = [line.strip() for line in lines if not line.startswith(';')]
 
-    string = ''.join(lines).replace('][', '],[')
-    string = re.sub("\s+", ",",string.strip())
+    string = ' '.join(lines)
+    string = re.sub(r'([\d\.]+|"[\w\/\s:&]+"|\]|[^\W\s]+)', r'\1,', string)
 
     return ast.literal_eval('['+string+']')
 
 
+def convert_terrain(in_path, out_path):
 
+    with open(in_path) as f:
+        string = netlogo_representation([line.strip().split() for line in f.readlines()])
+
+    string = re.sub(r'"([\d\.])"', r'\1', string)
+
+    with open(out_path, 'w') as f:
+        f.write(string)
