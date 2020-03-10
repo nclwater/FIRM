1b5c6359fb5f3c1a555dbc1ddc4a6d8ed1ee4e39
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2020-03-10 11:31:37 +0000

    reprojected roads

--- utils.py
+++ utils.py
@@ -2,7 +2,8 @@ import pprint
 import ast
 import re
 import xml.etree.ElementTree as ET
-
+import geopandas as gpd
+from shapely.geometry import Point
 
 def netlogo_representation(sequence: list, **kwargs):
     """
@@ -37,7 +38,7 @@ def convert_terrain(in_path, out_path):
         f.write(string)
 
 
-def convert_roads(in_path, out_path):
+def convert_roads(in_path, out_path, epsg=21096):
     with open(in_path) as f:
         tree = ET.fromstring(f.read())
 
@@ -57,7 +58,13 @@ def convert_roads(in_path, out_path):
             lons.append(float(attrib['lon']))
             ids.append(node_id)
 
-        roads.append([highway_id, ids[0], ids[-1], 0, "Road Type", list(zip(lons, lats))])
+        gdf = gpd.GeoDataFrame({'id': ids,
+                                'lat': lats,
+                                'lon': lons,
+                                'geometry': [Point(lon, lat) for lon, lat in zip(lons, lats)]
+                                }, crs={'init': 'epsg:4326'}).to_crs(epsg=epsg)
+
+        roads.append([highway_id, ids[0], ids[-1], 0, "Road Type", list(map(list, zip(gdf.geometry.x, gdf.geometry.y)))])
 
 
     with open(out_path, 'w') as f:
