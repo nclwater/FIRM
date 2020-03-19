fa26f239bafa601250b1488a545b7356c44bc95a
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2020-03-19 11:46:21 +0000

    using unittest in test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,22 @@
 import utils
+import unittest
 
-utils.read_netlogo_file('tests/towyn/agents.txt')
 
-utils.convert_terrain('tests/bwaise/terrain.asc', 'tests/bwaise/terrain.txt')
+class TestUtils(unittest.TestCase):
 
-utils.convert_roads('tests/bwaise/bwaise.osm', 'tests/bwaise/roads.txt')
+    def test_read_netlogo_file(self):
+        utils.read_netlogo_file('tests/towyn/agents.txt')
 
-utils.convert_buildings('tests/bwaise/bwaise.osm',
+    def test_convert_terrain(self):
+        utils.convert_terrain('tests/bwaise/terrain.asc', 'tests/bwaise/terrain.txt')
+
+    def test_convert_roads(self):
+        utils.convert_roads('tests/bwaise/bwaise.osm', 'tests/bwaise/roads.txt')
+
+    def test_convert_buildings(self):
+        utils.convert_buildings('tests/bwaise/bwaise.osm',
                         'tests/bwaise/roads.txt', 'tests/bwaise/preprocessed-buildings.txt')
+
+
+if __name__ == '__main__':
+    unittest.main()
